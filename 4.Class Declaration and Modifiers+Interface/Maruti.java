dc36067fe706a0437d82c4ef88c7f18fe5af72ee
paraswadhwa1801 <paras.18jan@gmail.com>
2020-12-02 21:20:38 +0530

    Added code snippet for better understanding of interface(Java 8)

--- 4.Class Declaration and Modifiers+Interface/Maruti.java
+++ 4.Class Declaration and Modifiers+Interface/Maruti.java
@@ -1,10 +1,19 @@
 import com.trial.package1.Car;
-class Maruti /*extends Car*/{           //cann't extend final class
+class Maruti implements Checkable/*extends Car*/{           //cann't extend final class
 
+    public void print(){
+        System.out.println("Needs to be overridden coz its an abstract method of interface");
+    }
+    // public void defaultMethod(){
+    //     System.out.println("Its optional to override default methods of interface");
+    // }
     public static void main(String... s) {
         Car car=new Car();              //Final class object can be created
         System.out.println(car.x);      //can access public variables & methods
        /* System.out.println(car.y);    //cann't access y coz it has default access 
                                         i.e. can be accessed in same package*/
+        Maruti maruti=new Maruti();
+        maruti.defaultMethod();
+        maruti.print();
     }
 }
