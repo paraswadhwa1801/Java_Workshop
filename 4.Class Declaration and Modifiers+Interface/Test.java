1be18554a8e07fe4dc1ff9446ccf68b575071d7a
paraswadhwa1801 <paras.18jan@gmail.com>
2020-12-02 14:31:39 +0530

    Paras | Added code snippets to understand abstract keyword in depth

--- 4.Class Declaration and Modifiers+Interface/Test.java
+++ 4.Class Declaration and Modifiers+Interface/Test.java
@@ -2,9 +2,18 @@ import static java.lang.System.out;
 public abstract class Test {
 
     abstract void method();     //atleast one abstact method is necessary in abstract class
+    static void nonAbstractMethod(){
+        out.println("Abstract class can have normal methods which are not mandatory to be overridden");
+    }
+    void nonStaticMethod(){
+        out.println("This method cann't be called from the same class but can be executed by its subclass");
+    }
     public static void main(String[] args) {
         //Test test=new Test();
+        //test.nonStaticMethod(); //not possible
         out.println("Abstract class cann't be instantiated");
+        nonAbstractMethod();
+        
     }
     
 }
